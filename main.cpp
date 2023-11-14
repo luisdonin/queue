8894e838809ebce7715f4f42a96638a12eeb848b
J. L. B. Donin <88415069+luisdonin@users.noreply.github.com>
2023-11-14 15:10:40 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-//#include <iostream>
-//#include <queue>
-//#include <string>
-//#include <functional>
+#include <iostream>
+#include <queue>
+#include <string>
+#include <functional>
 #include "student.h"
-//using namespace std;
+using namespace std;
 
 int main(){
     priority_queue<Aluno> filaAlunos;
